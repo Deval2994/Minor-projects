b14be0e67d84944730ab77f880cada0e2e078758
deval <deval.patel5@dcmail.ca>
2024-05-03 13:31:26 -0400

    finished

--- MergePDFs.py
+++ MergePDFs.py
@@ -1,13 +1,30 @@
+from PyPDF2 import PdfMerger
 import os
 
 
-def getDir():
-    path = input("Copy and past the directory here: ")
-    path = path.replace("\\", "/")
+def getDir(promt):
+    path = input(promt)
+    path = path.replace("\\", "/")  # Replace backslashes with forward slashes for consistency
     return path
 
-def getFile():
-    file = input("Copy and past the name of file only: ")
 
-os.chdir(f'{getDir()}')
-print(os.getcwd())
+def getFiles():
+    numberOfFiles = int(input("List total number of files to merge: "))
+    for i in range(1, numberOfFiles + 1):
+        file = input(f"Copy and paste path of file {i} (Do not include ' \" '): ")
+        file.replace("\\", "/") # Replace backslashes with forward slashes for consistency
+        pdfs.append(file)
+
+
+if __name__ == "__main__":
+    directory = getDir(
+        "Copy and paste the directory where you want to store merged PDF (Do not include ' \" '): ")  # Get the directory path from user input
+    pdfs = []  # Initialize an empty list to store PDF files
+    merger = PdfMerger()  # Initialize a PdfMerger object
+    getFiles()  # Get the list of PDF files to merge from user input
+    newPDF = input("Give a name to merged PDF: ")  # Prompt user to give a name to the merged PDF file
+    for pdf in pdfs:
+        merger.append(pdf)
+    newPDF = os.path.join(directory, newPDF)
+    merger.write(newPDF)
+    merger.close()
